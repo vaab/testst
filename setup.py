327698edef102b7ab76e43dc17ab7434c3cef33b
Valentin Lab <valentin.lab@kalysto.org>
2015-02-06 22:34:53 +0700

    try to get d2to1 out, to see if this is related to it being in the master package

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
 
-##
-## Normal d2to1 setup
-##
-
+from distutils.core import setup
 setup(
-    setup_requires=['d2to1'],
-    d2to1=True
-)
+    name='tests',
+    version='0.0.1',
+    package_dir={'': 'src'},
+    packages=['testst'],
+    install_requires=['kids.cache', ],
+    )
+
+
